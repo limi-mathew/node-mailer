8e00b8d934c5c731a4f700010b8fac7c3466fc64
limi-mathew <limimathew00@gmail.com>
2024-01-05 16:47:36 +0530

    Added Await Nodemailer files upload

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.use(express.json());
 
-app.post('/send-email', async (req, res) => {
-  try {
+ app.post('/send-email', async (req, res) => {
+//   try {
     // Extract data from the request body sent by React
     const { name, email, subject, message } = req.body;
 
@@ -15,7 +15,7 @@ app.post('/send-email', async (req, res) => {
       port: '587',
       auth: {
         user: 'limimathew00@gmail.com',
-        pass: 'xsmtpsib-d5748481f9076a9fdffd797b4a5ffcf39954e1fe88014d84f641eb691e3aa229-wpbX4ZtDIs6GPW10',
+        pass: 'xsmtpsib-d5748481f9076a9fdffd797b4a5ffcf39954e1fe88014d84f641eb691e3aa229-ApamvTIdBXrEVkDJ',
       },
     });
 
@@ -26,21 +26,19 @@ app.post('/send-email', async (req, res) => {
       text: `Name: ${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`,
     };
 
-    transporter.sendMail(mailOptions, function (error, info) {
+    await transporter.sendMail(mailOptions, function (error, info) {
       if (error) {
         console.error('Error:', error);
-        res.status(500).send('Internal Server Error');
       } else {
         console.log('Email sent successfully');
-        res.status(200).send('Email sent successfully');
       }
     });
 
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
+//   } catch (error) {
+//     console.error('Error:', error);
+//     res.status(500).send('Internal Server Error');
+//   }
+ });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
