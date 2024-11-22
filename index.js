bc4011f8e1f94d841668e30c93aa9a3bb95eb0b0
limi-mathew <limimathew00@gmail.com>
2024-11-22 16:12:10 +0530

    Console Issues

--- index.js
+++ index.js
@@ -9,23 +9,24 @@ app.use(cors({
   methods: 'POST',
   allowedHeaders: 'Content-Type',
 }));
- app.post('/send-email', async (req, res) => {
-  console.log(res,"res");
-  console.log("LIMI")
-  console.log(req,"requests");
-//   try {
-    // Extract data from the request body sent by React
-    const { name, email, subject, message } = req.body;
-     console.log(name,"name");
-    // Send email using nodemailer or an email service API
-    const transporter = nodemailer.createTransport({
-      host: 'smtp-relay.brevo.com',
-      port: '587',
-      auth: {
-        user: 'limimathew00@gmail.com',
-        pass: 'xsmtpsib-d5748481f9076a9fdffd797b4a5ffcf39954e1fe88014d84f641eb691e3aa229-7x5cJZX1VvjdBsap',
-      },
-    });
+console.log("limi")
+//  app.post('/send-email', async (req, res) => {
+//   console.log(res,"res");
+//   console.log("LIMI")
+//   console.log(req,"requests");
+// //   try {
+//     // Extract data from the request body sent by React
+//     const { name, email, subject, message } = req.body;
+//      console.log(name,"name");
+//     // Send email using nodemailer or an email service API
+//     const transporter = nodemailer.createTransport({
+//       host: 'smtp-relay.brevo.com',
+//       port: '587',
+//       auth: {
+//         user: 'limimathew00@gmail.com',
+//         pass: 'xsmtpsib-d5748481f9076a9fdffd797b4a5ffcf39954e1fe88014d84f641eb691e3aa229-7x5cJZX1VvjdBsap',
+//       },
+//     });
 
     const mailOptions = {
       from: email,
@@ -48,7 +49,7 @@ app.use(cors({
 //     console.error('Error:', error);
 //     res.status(500).send('Internal Server Error');
 //   }
- });
+ //});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
